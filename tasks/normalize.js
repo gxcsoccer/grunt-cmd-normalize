8f7b4b59b703b177a2000bf0040c54f827ed454c
gaoxiaochen <gxcsoccer@126.com>
2013-08-31 01:01:45 +0800

    init commit

---
+++ tasks/normalize.js
@@ -0,0 +1,13 @@
+/*
+ * grunt-cmd-normalize
+ *
+ * Copyright (c) 2013 GaoXiaochen
+ * Licensed under the MIT license.
+ */
+
+module.exports = function(grunt) {
+
+	grunt.registerMultiTask('normalize', 'Normalize everything into cmd.', function() {
+
+	});
+}

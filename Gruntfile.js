61ef69cba72727d248890ab54bbb690c7aa0f4c4
gaoxiaochen <gxcsoccer@126.com>
2013-09-01 00:57:21 +0800

    add some files

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+/**
+ * grunt-cmd-normalize
+ *
+ * Copyright (c) 2013 Peter Gao
+ */
+
+module.exports = function(grunt) {
+	grunt.initConfig({
+		jshint: {
+			all: [
+				'Gruntfile.js',
+				'tasks/**/*.js',
+				'<%= mochaTest.test.src %>'
+			],
+			options: {
+				jshintrc: '.jshintrc'
+			}
+		},
+		clean: {
+			expected: ['test/expected']
+		},
+		normalize: {
+			all: {
+				files: [{
+					expand: true,
+					cwd: 'test/cases/cmd',
+					src: ['**/*'],
+					dest: 'test/expected/cmd'
+				}]
+			}
+		},
+		mochaTest: {
+			test: {
+				options: {
+					reporter: 'spec'
+				},
+				src: ['test/normalize.js']
+			}
+		}
+	});
+
+	// load plugins
+	grunt.loadTasks('tasks');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-mocha-test');
+
+	grunt.registerTask('default', ['jshint']);
+	grunt.registerTask('test', ['clean', 'normalize', 'mochaTest', 'clean']);
+};
--- tasks/normalize.js
+++ tasks/normalize.js
@@ -8,6 +8,30 @@
 module.exports = function(grunt) {
 
 	grunt.registerMultiTask('normalize', 'Normalize everything into cmd.', function() {
+		var options = this.options({
 
+		});
+
+		var fname, destfile, count = 0;
+		this.files.forEach(function(fileObj) {
+			fileObj.src.forEach(function(fpath) {
+				count++;
+
+				if (fileObj.cwd) {
+					// not expanded
+					fname = fpath;
+					fpath = path.join(fileObj.cwd, fpath);
+				} else {
+					fname = path.relative(fileObj.orig.cwd || '', fpath);
+				}
+				if (grunt.file.isDir(fpath)) return;
+
+				destfile = path.join(fileObj.orig.dest || fileObj.dest, fname);
+
+				grunt.file.copy(fpath, destfile);
+			});
+		});
+
+		grunt.log.writeln('normalize ' + count.toString().cyan + ' files');
 	});
-}
+};
